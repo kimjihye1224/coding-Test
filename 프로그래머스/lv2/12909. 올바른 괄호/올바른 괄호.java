3bf7da00823acd40b77d287305cd07fbcf3b8e01
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2022-12-15 21:30:32 +0900

    Update 올바른 괄호.java

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -17,5 +17,27 @@ class Solution {
     	
     	if(!st.empty()) return false;
         return answer;
+        
+        
+        
+        
+    /*    boolean answer = false;
+        int count = 0;
+        for(int i = 0; i<s.length();i++){
+            if(s.charAt(i) == '('){
+                count++;
+            }
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count < 0){
+                break;
+            }
+        }
+        if(count == 0){
+            answer = true;
+        }
+
+        return answer;*/
     }
 }
