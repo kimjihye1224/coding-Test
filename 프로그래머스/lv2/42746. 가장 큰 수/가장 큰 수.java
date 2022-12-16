b8bb68ac7ed3d3f7e5aaa89705fa196bceed2b00
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2022-12-16 17:22:40 +0900

    [level 2] Title: 가장 큰 수, Time: 1.59 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+       String[] arr = new String[numbers.length];
+       
+       for(int i = 0; i < numbers.length; i++){
+           arr[i] = String.valueOf(numbers[i]);        
+       }
+       
+       Arrays.sort(arr, new Comparator<String>(){
+           @Override
+            public int compare(String n1, String n2) {
+                return (n2+n1).compareTo(n1+n2);
+            }
+        });
+       
+       for(int i = 0; i < arr.length; i++)
+           answer += arr[i];
+        
+       if(answer.charAt(0) == '0')
+    	   answer = "0";
+       
+        return answer;
+    }
+}
