c28909204b26bb626eb9a7e199d9d649e5afde1b
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-01-04 16:58:55 +0900

    Update 최소， 최대.cc

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -1,27 +1,35 @@
-#include <iostream>
+import java.util.*;
+import java.io.*;
 
-using namespace std;
+public class Main {
 
-int main(){
-	
-	int n;
-	cin>>n;
-	
-	int num[n+1];
-	int min = 1000000;
-	int max = -1000000;
-	
-	for(int i=0; i<n; i++){
-		cin >> num[i];
-		if(min > num[i]){
-			min = num[i];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		
+		int min = 1000000;
+		int max = -1000000;
+		
+		int num = 0;
+		for(int i = 0; i < n; i++) {
+			num = Integer.parseInt(st.nextToken());
+			if(num > max)
+				max = num;
+			
+			if(num < min)
+				min = num;
+			
+			//min = Math.min(min, num);
+			//max = Math.max(max, num);
 		}
-		if(max < num[i]){
-			max = num[i];
-		}
+		
+
+		System.out.println(min + " " + max);
+		
 	}
-	
-	cout << min << " " << max << "\n";
-	
-	return 0;
+
 }
+
