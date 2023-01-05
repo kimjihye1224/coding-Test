694158b3ceead68754b98622e8a13d0281228d6c
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-01-05 19:43:56 +0900

    Update 옹알이 （1）.java

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -20,6 +20,20 @@ class Solution {
         		answer++;
         }
         
+        
+        /*
+          for(int i =0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replace("aya", "1");
+            babbling[i] = babbling[i].replace("woo", "1");
+            babbling[i] = babbling[i].replace("ye", "1");
+            babbling[i] = babbling[i].replace("ma", "1");
+            babbling[i] = babbling[i].replace("1", "");
+            if(babbling[i].isEmpty()) {
+                answer = answer + 1;
+            }
+        }
+        
+        */
         return answer;
     }
 }
