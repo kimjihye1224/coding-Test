60a7b334dd6eb58ae89691788754fc345914db35
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-01-06 21:02:53 +0900

    Update 각도기.java

--- 프로그래머스/lv0/120829. 각도기/각도기.java
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -11,5 +11,6 @@ class Solution {
         else
             answer = 1;
         return answer;
+        //return angle == 180 ? 4 : angle < 90 ? 1 : angle == 90 ? 2 : angle > 90 ? 3 : 0;
     }
 }
