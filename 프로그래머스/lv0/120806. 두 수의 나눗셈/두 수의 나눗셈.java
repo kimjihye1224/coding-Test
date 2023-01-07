02c1b74aedf369da828cc529ce3b4c1e922e1cef
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-01-07 22:30:19 +0900

    Update 두 수의 나눗셈.java

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int num1, int num2) {
-        float result = (float)num1 / num2;
-        return (int)(result * 1000);
+        return (int)(((double)num1 / num2) * 1000);
     }
 }
