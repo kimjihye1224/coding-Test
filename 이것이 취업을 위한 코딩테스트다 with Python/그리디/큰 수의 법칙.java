07dc3cf45801a5584754192ce7b70eb2628795db
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-01-12 15:58:44 +0900

    Create 큰 수의 법칙.java

---
+++ 이것이 취업을 위한 코딩테스트다 with Python/그리디/큰 수의 법칙.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int[] number = new int[n];
+    
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			number[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(number);
+		
+		int result = 0;
+		for(int i = 1, j=1; i <= m; i++) {
+			if(j <= k) {
+				result += number[number.length - 1];
+				j++;
+			}
+			else {
+				result += number[number.length-2];
+				j = 1;
+			}
+		}
+		System.out.print(result);
+	}
+
+}
+
+
+--------------------------------------------------------------------------------------
+
+//저자 풀이
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // N, M, K를 공백을 기준으로 구분하여 입력 받기
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        // N개의 수를 공백을 기준으로 구분하여 입력 받기
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr); // 입력 받은 수들 정렬하기
+        int first = arr[n - 1]; // 가장 큰 수
+        int second = arr[n - 2]; // 두 번째로 큰 수
+
+        // 가장 큰 수가 더해지는 횟수 계산
+        int cnt = (m / (k + 1)) * k;
+        cnt += m % (k + 1);
+
+        int result = 0;
+        result += cnt * first; // 가장 큰 수 더하기
+        result += (m - cnt) * second; // 두 번째로 큰 수 더하기
+
+        System.out.println(result);
+    }
+
+}
