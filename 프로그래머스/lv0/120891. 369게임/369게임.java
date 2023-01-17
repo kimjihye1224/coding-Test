7ad649cd6348be6389bdba62479401879e1b4d1f
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-01-17 20:23:28 +0900

    Update 369게임.java

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -12,3 +12,6 @@ class Solution {
         return answer;
     }
 }
+
+
+return (int)Arrays.stream(String.valueOf(order).split("")).map(Integer::parseInt).filter(i -> i == 3 || i == 6 || i == 9).count();
