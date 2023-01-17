9f1e40a7bd7a4d520bb5785966e173fc14a46625
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-01-17 14:17:47 +0900

    Update 배열 회전시키기.java

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -15,3 +15,19 @@ class Solution {
         return answer;
     }
 }
+
+
+/*class Solution {
+    public int[] solution(int[] numbers, String direction) {
+        List<Integer> list = Arrays.stream(numbers).boxed().collect(Collectors.toList());
+
+        if (direction.equals("right")) {
+            list.add(0, list.get(list.size() - 1));
+            list.remove(list.size() - 1);
+        } else {
+            list.add(list.size(), list.get(0));
+            list.remove(0);
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}*/
