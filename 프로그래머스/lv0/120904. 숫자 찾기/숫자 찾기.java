3d99b2f3fe489a38a787eb461a5b90046af4759a
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-01-17 20:04:45 +0900

    [level 0] Title: 숫자 찾기, Time: 0.14 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int num, int k) {
+        String[] st = String.valueOf(num).split("");        
+        String n = String.valueOf(k);
+        
+        for(int i = 0; i < st.length; i++){
+            if(st[i].equals(n))
+                return i + 1;
+        }
+        return -1;
+    }
+}
