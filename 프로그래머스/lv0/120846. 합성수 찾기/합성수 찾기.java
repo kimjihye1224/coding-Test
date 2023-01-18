2bf3a1df63884b6862e1d3784e468291802b3312
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-01-18 14:21:51 +0900

    Update 합성수 찾기.java

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -18,3 +18,5 @@ class Solution {
         return answer;
     }
 }
+
+//return (int)IntStream.rangeClosed(1, n).filter(i -> (int)IntStream.rangeClosed(1, n).filter(i2 -> i % i2 == 0).count() > 2).count().
