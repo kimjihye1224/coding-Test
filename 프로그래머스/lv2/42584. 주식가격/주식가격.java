2be9744eaf27af839eb368f3010b8b7ef4467a2d
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-01-28 23:36:19 +0900

    Update 주식가격.java

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,19 +1,14 @@
 class Solution {
     public int[] solution(int[] prices) {
 		int[] answer = new int[prices.length];
-		int wait = 0, price = 0;
 		
-		for(int i = 0; i < prices.length-1; i++) {
-			wait = 0;
-			price = prices[i];
+		for(int i = 0; i < prices.length; i++) {
 			for(int j = i+1; j < prices.length; j++ ) {
-				wait++;
-				if(price > prices[j]) break;
+				answer[i]++;
+				if(prices[i] > prices[j]) break;
 			}
-			answer[i] = wait;
 		}
 		
-		answer[prices.length-1] = 0;
         return answer;
     }
 }
