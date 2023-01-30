0b269758dc10d597a871d73c60284dd9975ea95c
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-01-30 23:56:05 +0900

    [level 2] Title: 더 맵게, Time: 3538.62 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -4,20 +4,21 @@ class Solution {
     	int answer = 0;
     	
     	PriorityQueue<Integer> q = new PriorityQueue<>();
-    	for(int n : scoville)
-    		q.add(n);
-    	
-    	while(q.peek()<=K) {
-    		if(q.size() == 1) return -1;
-    		
-    		int a = q.poll();
-    		int b = q.poll();
-    		
-    		int result = a + (b * 2);
-    		
-    		q.offer(result);
-    		answer++;
-    	}
+        
+    	for(int n : scoville){
+    		q.offer(n);
+        }
+        
+        int count = 0;
+        
+        while(q.peek() < K) {
+            if(q.size() == 1) return -1;
+            
+        	q.offer( q.poll() + (q.poll() * 2));
+        	answer++;
+        	
+        }
+        
     	return answer;
     }
 }
