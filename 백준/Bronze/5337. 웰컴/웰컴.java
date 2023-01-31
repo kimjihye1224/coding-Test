de2ec7924fec39b6f7678c42d4af675e91192299
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-01-31 17:01:32 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+        
+	}
+
+}
