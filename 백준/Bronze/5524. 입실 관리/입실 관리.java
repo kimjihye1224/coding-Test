dfe08e899e06b0db4e95ec7899f86e9b4f617f89
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-02-02 20:08:26 +0900

    [Bronze IV] Title: 입실 관리, Time: 132 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine()); 
+        
+        int n = Integer.parseInt(st.nextToken());
+        
+        for(int i = 1; i <= n; i++){
+            String s = br.readLine();
+            sb.append(s.toLowerCase()  +'\n');
+        }
+       
+        System.out.print(sb.toString());
+        
+	}
+
+}
