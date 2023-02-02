c4d27938a4f77e89b3256c7dc412e796b83523b8
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-02-02 21:35:41 +0900

    Update K번째수.java

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -18,3 +18,21 @@ class Solution {
         return answer;
     }
 }
+
+
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        ArrayList<Integer> li = new ArrayList<>();
+        
+        for(int i = 0; i < commands.length; i++) {
+        	int[] a = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+        	Arrays.sort(a);
+        	answer[i] = a[commands[i][2] - 1];
+        }
+        
+        return answer;
+    }
+}
