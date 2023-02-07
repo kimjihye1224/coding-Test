d9c61db58ea0d1cc03801593b99402758e31a55d
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-02-07 21:14:05 +0900

    [level 1] Title: 최소직사각형, Time: 1.68 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/README.md
+++ 프로그래머스/lv1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 93.5 MB, 시간: 21.70 ms
+메모리: 89.6 MB, 시간: 1.68 ms
 
 ### 구분
 
--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -1,26 +1,12 @@
-import java.util.*;
+
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-                
-        List<Integer> li1 = new ArrayList<Integer>(); //가로
-        List<Integer> li2 = new ArrayList<Integer>(); //세로
-        
-        for(int[] size : sizes) {
-        	if(size[0] < size[1]) {
-        		li1.add(size[1]);
-        		li2.add(size[0]);
-        		
-        	}else {
-        		li1.add(size[0]);
-        		li2.add(size[1]);
-        	}
+        int length = 0, height = 0;
+        for (int[] card : sizes) {
+            length = Math.max(length, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
         }
-        
-        Collections.sort(li1);
-        Collections.sort(li2);
-        
-        answer = li1.get(li1.size() - 1) * li2.get(li2.size() - 1);
+        int answer = length * height;
         return answer;
     }
 }
