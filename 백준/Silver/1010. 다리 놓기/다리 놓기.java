851cc56ba1e1e2c201879e23bf0d2f4c2e0cb82f
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-02-23 23:06:33 +0900

    Update 다리 놓기.java

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -20,6 +20,40 @@ public class Main{
 
 		}
 	}
+	
+	
+//2번째 풀이	
+    private static final int[][] dp = new int[31][31];
+	
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	
+    	int t = Integer.parseInt(br.readLine());
+    	
+    	for(int i = 0; i < t; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		
+    		int n = Integer.parseInt(st.nextToken());
+    		int m = Integer.parseInt(st.nextToken());
+    		
+    		System.out.println(combination(m,n));
+    		
+    	}
+    	
+    	
+    }
+    
+    public static int combination(int n, int r)
+    {
+    	if(dp[n][r] > 0)
+    		return dp[n][r];
+    	else if(n == r || r == 0)
+    		return dp[n][r] = 1;
+    	else
+    		return combination(n-1, r-1) + combination(n-1,r);
+    	
+    }
 
 }
     
