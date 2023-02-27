5c394c4240bfb640026932f1c77d094924d2d0cc
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-02-28 00:21:17 +0900

    Update 평행사변형.java

--- 백준/Silver/1064. 평행사변형/평행사변형.java
+++ 백준/Silver/1064. 평행사변형/평행사변형.java
@@ -6,8 +6,6 @@ import java.util.stream.IntStream;
 
 public class Main {
 	
-	private static final int[][] dp = new int[31][31];
-	
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,11 +17,12 @@ public class Main {
     	int cx = Integer.parseInt(st.nextToken());
     	int cy = Integer.parseInt(st.nextToken());
     	
-    	
+    	//기울기 비교.
     	if((ay - by) * (ax - cx) == (ax-bx) * (ay - cy))
     		System.out.println("-1");
     	else{
-    		double ab = Math.sqrt(Math.pow((ax - bx),2) + Math.pow((ay-by),2));
+		//두 점 사이의 거리 통해 길이 구하기. 
+		double ab = Math.sqrt(Math.pow((ax - bx),2) + Math.pow((ay-by),2));
         	double bc = Math.sqrt(Math.pow((bx - cx),2) + Math.pow((by-cy),2));
         	double ac = Math.sqrt(Math.pow((ax - cx),2) + Math.pow((ay-cy),2));
         	
