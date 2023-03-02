555ae31584343c7d141744d244c72211d5b32460
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-03-02 23:21:20 +0900

    Update 단어 정렬.java

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -42,14 +42,33 @@ public class Main {
     	}
     	
     	System.out.println(sb);
-    	
-    		
-    	
+    	    	
     }
+}
 
-    
-    
-       
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String[] arr = new String[n];
+		
+		for(int i=0; i<n; i++)
+			arr[i] = br.readLine();
+		
+		Arrays.sort(arr, (o1, o2) -> {
+			return o1.length() == o2.length() ? o1.compareTo(o2) : o1.length() - o2.length();
+		});
+		
+		System.out.println(arr[0]);
+		for(int i=1; i<n; i++)
+			if(!arr[i].equals(arr[i-1]))
+				System.out.println(arr[i]);
+		
+		br.close();
+	}
 }
