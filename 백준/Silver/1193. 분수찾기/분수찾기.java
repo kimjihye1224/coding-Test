3bb1e94b81ff32e4dcef0f8789ace9e924b5e7fa
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-03-16 23:17:42 +0900

    Update 분수찾기.java

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -29,3 +29,39 @@ public class Main {
     	}
     }
 }
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int X = Integer.parseInt(br.readLine());
+ 
+		int cross_count = 1, prev_count_sum = 0;
+ 
+		while (true) {
+			if (X <= prev_count_sum + cross_count) {	
+				
+				if (cross_count % 2 == 1) {	// 대각선의 개수가 홀수라면 
+					System.out.print((cross_count - (X - prev_count_sum - 1)) + "/" + (X - prev_count_sum));
+					break;
+				} 
+				
+				else {	// 대각선의 개수가 짝수라면 
+					System.out.print((X - prev_count_sum) + "/" + (cross_count - (X - prev_count_sum - 1)));
+					break;
+				}
+ 
+			} else {
+				prev_count_sum += cross_count;
+				cross_count++;
+			}
+		}
+	}
+}
