805fa6d99eab154142af2d35c9b4ff2e8ed3b971
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-04-05 20:52:27 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,26 +1,18 @@
-import java.io.*;
-import java.util.*;
 class Solution {
     public int solution(int[] ingredient) {
-		Stack<Integer> st = new Stack<>();
-		int count = 0;
-		
-		for(int i = 0; i < ingredient.length; i++) {
-			st.push(ingredient[i]);
-			int size = st.size();
-			
-			if(size > 3 && st.peek() == 1 
-			&& st.get(size - 2 )== 3
-			&& st.get(size - 3) == 2
-			&& st.get(size - 4) == 1) {
-				st.pop();
-				st.pop();
-				st.pop();
-				st.pop();
-				count++;
-			}
-		}
-		
-        return count;
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
+        int answer = 0;
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
+                answer++;
+            }
+        }
+        return answer;
     }
 }
