667527352fda4bc63c2f55c47bd322e52e125090
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-04-11 19:31:04 +0900

    Update 3진법 뒤집기.java

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -3,16 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         StringBuilder sb = new StringBuilder();
-		while(n > 0) {
-			sb.append(n % 3);
-			n /= 3;
-			
-		}
+	int result = 0;
+	    
+	//바로 0021로 구해짐. 
+	while(n > 0) {
+		sb.append(n % 3);
+		n /= 3;		
+	}
 		
-		int result = 0;
-		for(int i = 0; i < sb.length(); i++) {
-			result += (sb.charAt(sb.length() - (i+1)) - '0') * Math.pow(3, i);
-		}
+	//return Integer.parseInt(a,3); 로 바로 구할 수 있음. 
+	for(int i = 0; i < sb.length(); i++) {
+		result += (sb.charAt(sb.length() - (i+1)) - '0') * Math.pow(3, i);
+	}
         return result;
     }
 }
