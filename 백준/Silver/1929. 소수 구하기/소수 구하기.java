18bd56f38f370b0a5c8c4e759fc70074b80d6bb8
kimjihye1224 <71050860+kimjihye1224@users.noreply.github.com>
2023-04-17 17:34:14 +0900

    [Silver III] Title: 소수 구하기, Time: 224 ms, Memory: 20020 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static boolean[] prime;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+        
+		prime = new boolean[n+1];
+		find();
+		
+		for(int i = m; i <= n; i++) {
+			if(!prime[i]) sb.append(i).append('\n');
+		}
+		System.out.print(sb);
+	}
+	
+	public static boolean find() {
+		prime[0] = prime[1] = true;
+		
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if(prime[i]) continue;
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
+		
+		return true;
+	}
+	
+}
